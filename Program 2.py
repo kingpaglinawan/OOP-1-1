82e094346e8d9ff40e20c35cc69f089902c93804
KING JOHN ADAMZ R. PAGLINAWAN <90814383+kingpaglinawan@users.noreply.github.com>
2022-04-25 15:30:51 +0800

    Add files via upload

---
+++ Program 2.py
@@ -0,0 +1,22 @@
+from tkinter import *
+window = Tk()
+
+window.geometry("600x500+30+20")
+window.title("Midterm in OOP")
+
+lbl = Label(window, text="Enter your fullname:", fg="red")
+lbl.place(x=80, y=100)
+
+txtfld = Entry(window, bd=3, font=("Arial", 16))
+txtfld.place(x=300, y=100)
+
+def click():
+    value=txtfld.get()
+    txtfld2.insert(END,str(value))
+btn = Button(window, text = "Click to display your Fullname", fg="red", command=click)
+btn.place(x=80, y=150)
+
+txtfld2 = Entry(window, bd=3, font=("Arial", 16))
+txtfld2.place(x=300, y=150)
+
+window.mainloop()
